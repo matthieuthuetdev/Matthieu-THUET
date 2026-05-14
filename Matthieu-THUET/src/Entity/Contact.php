cff2455d19435afbea97168330f80f0c948ce5ef
matthieuthuetdev <mthuet.pro@gmail.com>
2026-05-14 23:27:13 +0200

    create contact entity

---
+++ Matthieu-THUET/src/Entity/Contact.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContactRepository::class)]
+class Contact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?string $companyName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $emailAddress = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $subject = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Content = null;
+
+    #[ORM\Column]
+    private ?bool $RGPD = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getfirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setfirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCompanyName(): ?string
+    {
+        return $this->companyName;
+    }
+
+    public function setCompanyName(?string $companyName): static
+    {
+        $this->companyName = $companyName;
+
+        return $this;
+    }
+
+    public function getEmailAddress(): ?string
+    {
+        return $this->emailAddress;
+    }
+
+    public function setEmailAddress(string $emailAddress): static
+    {
+        $this->emailAddress = $emailAddress;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): static
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->Content;
+    }
+
+    public function setContent(string $Content): static
+    {
+        $this->Content = $Content;
+
+        return $this;
+    }
+
+    public function isRGPD(): ?bool
+    {
+        return $this->RGPD;
+    }
+
+    public function setRGPD(bool $RGPD): static
+    {
+        $this->RGPD = $RGPD;
+
+        return $this;
+    }
+}
