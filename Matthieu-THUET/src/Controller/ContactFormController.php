1a4c9e5b016eb2be7890d4b79c7920d60531fb6f
matthieuthuetdev <mthuet.pro@gmail.com>
2026-05-16 23:13:04 +0200

    create the contact form

--- Matthieu-THUET/src/Controller/ContactFormController.php
+++ Matthieu-THUET/src/Controller/ContactFormController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactFormController extends AbstractController
 {
     #[Route('/contact/form', name: 'app_contact_form')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre message a bien été envoyé.');
+            return $this->redirectToRoute('app_contact_form');
+        }
+
         return $this->render('contact_form/index.html.twig', [
-            'controller_name' => 'ContactFormController',
+            'contactForm' => $form->createView(),
         ]);
     }
 }
---
+++ Matthieu-THUET/src/Form/ContactType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('companyName', TextType::class, [
+                'label' => 'Entreprise',
+                'required' => false,
+            ])
+            ->add('emailAddress', EmailType::class, [
+                'label' => 'E-mail',
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Sujet',
+            ])
+            ->add('Content', TextareaType::class, [
+                'label' => 'Message',
+            ])
+            ->add('RGPD', CheckboxType::class, [
+                'label' => "J'accepte la politique de confidentialité (RGPD)",
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
