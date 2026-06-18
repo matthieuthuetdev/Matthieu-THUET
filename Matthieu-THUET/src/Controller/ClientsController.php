aca3092f0dda3e646f8e2d1603326f35c739c58a
matthieuthuetdev <mthuet.pro@gmail.com>
2026-06-18 22:35:37 +0200

    Refactor the clients page to use the shared catalog

--- Matthieu-THUET/src/Controller/ClientsController.php
+++ Matthieu-THUET/src/Controller/ClientsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\ClientsCatalog;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ClientsController extends AbstractController
 {
     #[Route('/clients', name: 'app_clients')]
-    public function index(): Response
+    public function index(ClientsCatalog $clientsCatalog): Response
     {
         return $this->render('clients/index.html.twig', [
             'controller_name' => 'ClientsController',
+            'clients' => $clientsCatalog->all(),
         ]);
     }
 }
